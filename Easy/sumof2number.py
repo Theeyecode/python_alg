1408249b446d4e3e9cc6435314b75d668fb927ff
theeyecode <emmanuelolajubu90@gmail.com>
2021-10-18 15:17:13 +0100

    BST

--- Easy/sumof2number.py
+++ Easy/sumof2number.py
@@ -1,15 +1,32 @@
+# def sumOfTwoNumbers(array, targetSum):
+#     nums = {}
+
+#     for n in array:
+#         potentialNum = targetSum - n
+#         if potentialNum in nums:
+#             return [potentialNum, n]
+
+#         else:
+#             nums[n] = True
+
+#     return []
+
 def sumOfTwoNumbers(array, targetSum):
-    nums = {}
+    array.sort()
+    left = 0
+    right = len(array) - 1
 
-    for n in array:
-        potentialNum = targetSum - n
-        if potentialNum in nums:
-            return [potentialNum, n]
-        else:
-            nums[n] = True
+    while left < right:
+        currentsum = array[left] + array[right]
 
+        if currentsum == targetSum:
+            return[array[left], array[right]]
+        elif currentsum < targetSum:
+            left += 1
+        elif currentsum > targetSum:
+            right -= 1
     return []
 
 
-x = sumOfTwoNumbers([4, 6, -3, 9, 12, 2, 1, 6], 13)
+x = sumOfTwoNumbers([6, -3, 9, 1, 22, 1, 6, 12], 13)
 print(x)
