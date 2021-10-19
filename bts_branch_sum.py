aa7eb4901c35591e96a8041d86f286eea323c872
theeyecode <emmanuelolajubu90@gmail.com>
2021-10-19 11:42:51 +0100

    first_recursive_code

---
+++ bts_branch_sum.py
@@ -0,0 +1,25 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    sums = []
+    calculateBranchSum(root, 0, sums)
+    return sums
+    # Write your code here.
+
+
+def calculateBranchSum(node, runningSum, sums):
+    if node is None:
+        return
+
+    newRunningSum = runnimgSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+
+    calculateBranchSum(node.left, newRunningSum, sums)
+    calculateBranchSum(node.right, newRunning, sums)
