bfbbabda35a2d2bee8132238748bd6cc8db827d5
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-09 23:41:12 +0100

    convert

--- stack.py
+++ stack.py
@@ -20,6 +20,8 @@ class Stack():
     def get_stack(self) -> None:
         return self.items
 
+# python code to reverse a string using stack.
+
 
 def reverse_string(stack, input_str) -> None:
     for i in range(len(input_str)):
@@ -31,13 +33,25 @@ def reverse_string(stack, input_str) -> None:
     return rev_string
 
 
+# python code to convert binary numbers to integer
+
+def convert_decimal(dec_num=int) -> None:
+    new_stack = Stack()
+    while dec_num > 0:
+        remainder = dec_num % 2
+        new_stack.push(remainder)
+        dec_num = dec_num // 2
+    bin_num = ""
+    while not new_stack.is_empty():
+        bin_num += str(new_stack.pop())
+    return bin_num
+
+
 s = Stack()
 s.push(1)
 s.push(2)
 s.push(3)
 s.push(4)
 s.push(5)
-stack = Stack()
-string = 'Hello'
 
-print(reverse_string(stack, string))
+print(convert_decimal(242))
