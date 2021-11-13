1e84e347f7faa53e2bccc2e21cd34b3bd91f650d
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-13 13:17:26 +0100

    delete

--- Recursion/len_string.py
+++ Recursion/len_string.py
@@ -1,10 +1,17 @@
 # This code calculates the length of an input string
 
-def length_string_recursive(input_string):
+def length_string_iterative(input_string):
     count = 0
     for i in input_string:
         count += 1
     return count
 
 
+def length_string_recursive(input_string, idx=0):
+    if input_string == '':
+        return False
+    return 1 + length_string_recursive(input_string[1:])
+
+
+print(length_string_iterative("abc"))
 print(length_string_recursive("abc"))
