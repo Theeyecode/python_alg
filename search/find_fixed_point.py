45cd309abd2878f5dee977e80fd90a4d153900c3
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-14 12:57:46 +0100

    linear

--- search/find_fixed_point.py
+++ search/find_fixed_point.py
@@ -1,8 +1,9 @@
+# time complexity is 0(logn)
 def find_fixed_point(data):
     low = 0
     high = len(data) - 1
 
-    while low < high:
+    while low <= high:
         mid = (low + high)//2
         if mid == data[mid]:
             return data[mid]
@@ -18,7 +19,18 @@ def find_fixed_point(data):
                 return False
     return False
 
+# brute approach
 
-A = [-10, -5, 0, 8, 7]
+
+def find_fixed_point_linear(data):
+    for i in range(len(data)):
+        if i == data[i]:
+            return data[i]
+    return False
+
+
+A = [-10, -5, 0, 3, 7]
 y = find_fixed_point(A)
+x = find_fixed_point_linear(A)
 print(y)
+print(x)
