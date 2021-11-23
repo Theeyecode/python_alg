d2e6e598b0e5661d862bf69f3c211707fd6d5d1f
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-23 15:40:16 +0100

    generte_document

---
+++ Easy/generate_document.py
@@ -0,0 +1,23 @@
+def generateDocument(characters, document):
+    if len(characters) != len(document):
+        return False
+    d = dict()
+
+    for char in characters:
+        if char in d:
+            d[char] += 1
+        else:
+            d[char] = 1
+
+    for doc in document:
+        if doc in d:
+            d[doc] -= 1
+        else:
+            d[doc] = 1
+
+    return all(value == 0 for value in d.values())
+
+
+y = generateDocument("Bst AEterpx sihet elog!", "AlgoExpert is the Best!")
+# y = generateDocument("A", "A")
+print(y)
