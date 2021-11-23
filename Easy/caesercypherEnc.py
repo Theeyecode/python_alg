85f0a2ed63518ac3993ebefa770065c4bdf39be4
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-23 10:18:23 +0100

    correction

--- Easy/caesercypherEnc.py
+++ Easy/caesercypherEnc.py
@@ -2,8 +2,9 @@ def caesarCipherEncryptor(string, key):
     if key == 0:
         return string
     newLetters = []
+    newkey = key % 26
     for i in string:
-        newLetters.append(helperfunction(i, 2))
+        newLetters.append(helperfunction(i, newkey))
     return "".join(newLetters)
 
 
@@ -12,5 +13,5 @@ def helperfunction(letter, key):
     return chr(newLetter) if newLetter <= 122 else chr(96 + newLetter % 122)
 
 
-y = caesarCipherEncryptor("xyz", 2)
+y = caesarCipherEncryptor("xyz", 54)
 print(y)
