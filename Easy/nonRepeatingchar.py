31824b6c12296beb16e8ecfd726446c358b9fdb5
theeyecode <emmanuelolajubu90@gmail.com>
2021-11-23 23:06:02 +0100

    non_repeating

--- Easy/nonRepeatingchar.py
+++ Easy/nonRepeatingchar.py
@@ -13,5 +13,18 @@ def firstNonRepeatingCharacter(string):
     return -1
 
 
-y = firstNonRepeatingCharacter('abcaa')
+def secondfunction(string):
+    foundDuplicate = True
+    for idx in range(len(string)):
+        foundDuplicate = False
+        for idx2 in range(idx, len(string)):
+            if string[idx] == string[idx2] and idx != idx2:
+                foundDuplicate = True
+
+        if not foundDuplicate:
+            return idx
+    return -1
+
+
+y = secondfunction('akbcaka')
 print(y)
