14993ee703f8fc8bdc93d863a9dd09cf307287c9
theeyecode <emmanuelolajubu90@gmail.com>
2021-12-08 07:14:53 +0100

    DFS

---
+++ Easy/depth_first_chile.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        array.append(self.name)
+
+        for x in self.children:
+            x.depthFirstSearch(array)
+        return array
+
+    pass
