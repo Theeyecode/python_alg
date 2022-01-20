a7bfa4b3aa30e9fa90d69b1066d24a9da60aaa26
theeyecode <emmanuelolajubu90@gmail.com>
2022-01-20 11:13:20 +0100

    knn_new

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-i = 1
-j = 4
+my_tuppy = (1,2,5,8)
 
-
-def text(i, j):
-    while not i < j:
-        print("okay")
-        i += 1
-    print('not less')
-    i += 1
-    print(i)
-
-
-print(text(i, j))
+my_tuppy[2] = 6
