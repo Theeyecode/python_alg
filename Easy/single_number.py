ab9372d520b59e37d7e080272eb769fa1b6e945e
Olajubu Emmanuel <emmanuelolajubu90@gmail.com>
2022-09-02 17:12:50 +0100

    recursion

---
+++ Easy/single_number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return seen.pop()
