be531c62cfdeb4d5ee1885fcacb40c855dab7f2e
Emmanuel Olajubu <emmanuelolajubu90@gmail.com>
2023-02-20 17:42:46 +0100

    Create countPalindomicSubsequence.py

---
+++ LeeC/easy/countPalindomicSubsequence.py
@@ -0,0 +1,22 @@
+import collections
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        left = set()
+        res = set()
+        right = collections.Counter(s)
+        # print(right)
+
+        for i in range(len(s)):
+            right[s[i]]-=1
+            if right[s[i]] == 0:
+                right.pop(s[i])
+            
+            for j in range(26):
+                c = chr(ord("a") +j)
+                if c in left and c in right:
+                    res.add((s[i], c))
+                    # print(res)
+            left.add(s[i])
+        return len(res)
+
+            
