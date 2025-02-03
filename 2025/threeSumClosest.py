2597e02d767018d4331346d4ba79ee29969fb4d3
Emmanuel Olajubu <emmanuelolajubu@Emmanuels-MacBook-Pro.local>
2025-02-02 22:44:08 -0500

    combinationSum

--- 2025/threeSumClosest.py
+++ 2025/threeSumClosest.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         diff = float('inf')
--- Medium/partitionList.dart
+++ Medium/partitionList.dart
@@ -1,24 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        d1 = c1 = ListNode(0)
-        d2 = c2 = ListNode(0)
+// # Definition for singly-linked list.
+// # class ListNode:
+// #     def __init__(self, val=0, next=None):
+// #         self.val = val
+// #         self.next = next
+// class Solution:
+//     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+//         d1 = c1 = ListNode(0)
+//         d2 = c2 = ListNode(0)
 
-        while head:
-            if head.val<x:
-                c1.next = head
-                c1 = c1.next
+//         while head:
+//             if head.val<x:
+//                 c1.next = head
+//                 c1 = c1.next
 
-            else:
-                c2.next =head
-                c2=c2.next
+//             else:
+//                 c2.next =head
+//                 c2=c2.next
 
-            head = head.next
+//             head = head.next
         
-        c2.next = None
-        c1.next = d2.next
-        return d1.next
+//         c2.next = None
+//         c1.next = d2.next
+//         return d1.next
