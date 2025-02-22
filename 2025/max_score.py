edba1e5a96588e44073b78c6d792c4fb5997705c
Emmanuel Olajubu <emmanuelolajubu@Emmanuels-MacBook-Pro.local>
2025-02-21 20:19:57 -0500

    optimized maxScore problem

--- 2025/max_score.py
+++ 2025/max_score.py
@@ -32,3 +32,18 @@ class Solution:
         return res
         
         #timeComplexity is 0(n2) space complexity is 0(1)
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        one = s.count('1')
+        zero = 0
+        res = 0
+        for i in range(len(s)-1):
+            if s[i] == '0':
+                zero+=1
+            else:
+                one-=1
+            res = max(res,zero+one)
+        return res
+
+        # time Complexity = 0(n), space complexity 0(1)
